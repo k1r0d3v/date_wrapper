6602576fb90b26c125c7090e789a25efaa2ac2c0
k1r0d3v <k1r0d3v@gmail.com>
2022-05-26 20:52:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+A wrapper around DateTime to simplify the use of dates without time.
+
+## Installing
+
+1. Add dependencies to `pubspec.yaml`
+```yaml
+dependencies:
+     calendar_view: <latest-version>
+```
+2. Run pub get
+```
+flutter pub get
+```
+
+3. Import package
+```dart
+import 'package:date_wrapper/date.dart';
+```
+
+## Usage
+
+Usage similar to `DateTime`:
+
+```dart
+final now = DateTime.now();
+final today = Date.today();
+
+final tomorrow = today.add(const Duration(days: 1));
+final yesterday = now.date.subtract(const Duration(days: 1));
+```
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Alejandro Romero
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ lib/src/date.dart
@@ -0,0 +1,105 @@
+/// Date class that wraps [DateTime].
+class Date {
+  // Weekday constants
+  static const int monday = DateTime.monday;
+  static const int tuesday = DateTime.tuesday;
+  static const int wednesday = DateTime.wednesday;
+  static const int thursday = DateTime.thursday;
+  static const int friday = DateTime.friday;
+  static const int saturday = DateTime.saturday;
+  static const int sunday = DateTime.sunday;
+  static const int daysPerWeek = DateTime.daysPerWeek;
+
+  /// The [DateTime] that wraps this class.
+  ///
+  /// This time part of this [DateTime] is ignored in the operations,
+  /// and defaults to 0 in its construction.
+  final DateTime dateTime;
+
+  /// Constructs a [Date] instance, refer to [DateTime] to more information.
+  ///
+  /// For example, to create a [Date] object:
+  /// ```dart
+  /// final myFavouriteDay = Date(1994, 9, 24);
+  /// ```
+  Date(int year, int month, int day) : dateTime = DateTime(year, month, day);
+
+  /// Constructs a new [Date] instance based on [dateTime].
+  ///
+  /// This takes the date part of the given [DateTime]
+  /// and discards the rest.
+  Date.of(DateTime dateTime)
+      : dateTime = DateTime(dateTime.year, dateTime.month, dateTime.day);
+
+  /// Constructs a [Date] instance with current date.
+  ///
+  /// ```dart
+  /// final now = Date.today();
+  /// ```
+  Date.today() : dateTime = Date.of(DateTime.now()).dateTime;
+
+  /// Returns a human-readable string for this instance.
+  ///
+  /// This method takes the date part of [DateTime.toString].
+  @override
+  String toString() => dateTime.toString().split(' ')[0];
+
+  /// Returns true if [other] is a [Date] at the same date.
+  ///
+  /// Refer to [DateTime] for more information.
+  @override
+  bool operator ==(Object other) {
+    if (other is Date) {
+      return dateTime == other.dateTime;
+    } else {
+      return this == other;
+    }
+  }
+
+  /// Refer to [DateTime] for more information.
+  @override
+  int get hashCode => dateTime.hashCode;
+
+  /// Refer to [DateTime] for more information.
+  int get year => dateTime.year;
+
+  /// Refer to [DateTime] for more information.
+  int get month => dateTime.month;
+
+  /// Refer to [DateTime] for more information.
+  int get day => dateTime.day;
+
+  /// Refer to [DateTime] for more information.
+  int get weekday => dateTime.weekday;
+
+  /// Returns the last day of this [Date] month.
+  int get lastDayOfMonth => Date(year, month + 1, 0).day;
+
+  /// Refer to [DateTime] for more information.
+  bool isBefore(Date other) => dateTime.isBefore(other.dateTime);
+
+  /// Refer to [DateTime] for more information.
+  bool isAfter(Date other) => dateTime.isAfter(other.dateTime);
+
+  /// Refer to [DateTime] for more information.
+  int compareTo(Date other) => dateTime.compareTo(other.dateTime);
+
+  /// Refer to [DateTime] for more information.
+  Duration difference(Date other) => dateTime.difference(other.dateTime);
+
+  /// Refer to [DateTime] for more information.
+  Date add(Duration duration) => Date.of(dateTime.add(duration));
+
+  /// Refer to [DateTime] for more information.
+  Date subtract(Duration duration) => Date.of(dateTime.subtract(duration));
+
+  /// Refer to [DateTime] for more information.
+  Date copyWith({int? year, int? month, int? day}) =>
+      Date(year ?? this.year, month ?? this.month, day ?? this.day);
+}
+
+/// Extension on [DateTime] to easily transform [DateTime] to [Date].
+///
+extension DateExtension on DateTime {
+  Date get date => Date.of(this);
+}
